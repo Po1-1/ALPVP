2b37b6daab3cdea15ca40fe712eb696b1cd5b776
Owennn04 <owentengkawan@gmail.com>
2025-12-13 00:50:52 +0700

    Initialize

--- app/src/main/java/com/hendra/alpvp/data/util/NetworkUtils.kt
+++ app/src/main/java/com/hendra/alpvp/data/util/NetworkUtils.kt
@@ -7,18 +7,18 @@ suspend fun <T> safeApiCall(call: suspend () -> Response<T>): Result<T> {
     return try{
         val response = call()
         if (response.isSuccessful && response.body() != null){
-            Result.success(response.body()!!)
+            Result.success(response.body()!!) // Untuk mengembalikan data jika berhasil
         }else{
-            val errorBody = response.errorBody()?.string()
-            val errorMessage = try{
-                val jsonObject = JSONObject(errorBody ?: "")
-                jsonObject.getString("message")
-            } catch (e: Exception) {
-                "Terjadi Kesalahan: ${response.code()} ${response.message()}"
+            val errorBody = response.errorBody()?.string() // Jika gagal, ambil pesan kesalahan dari body
+            val errorMessage = try{ // Asumsi format error server: { "message": "Password salah", ... }
+                val jsonObject = JSONObject(errorBody ?: "") // Parsing JSON untuk pesan kesalahan
+                jsonObject.getString("message") // Mengambil pesan kesalahan dari JSON
+            } catch (e: Exception) { // Jika gagal, gunakan pesan kesalahan dari body
+                "Terjadi Kesalahan: ${response.code()} ${response.message()}" // Jika gagal, gunakan pesan kesalahan dari body
             }
             Result.failure(Exception(errorMessage))
         }
-    } catch (e:Exception){
+    } catch (e:Exception){ // Jika terjadi error koneksi/jaringan (misal: tidak ada internet)
         Result.failure(Exception("Gagal Terhubung ke server. Cek Koneksi "))
     }
 }
